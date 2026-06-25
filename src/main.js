d2d8c0091ffcf06bcc89f7618c5a303809d360ad
Zuchi97 <eddy050315@gmail.com>
2026-06-25 17:37:39 +0800

    Initial Phaser setup

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Phaser from "phaser";
+import BattleScene from "./game/scenes/BattleScene";
+
+const config = {
+
+    type: Phaser.AUTO,
+
+    width: 1280,
+
+    height: 720,
+
+    backgroundColor: "#202020",
+
+    scene: [
+        BattleScene
+    ]
+};
+
+new Phaser.Game(config);
